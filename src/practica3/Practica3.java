9f4508672dad613a27b9e9946bafa103417cf29e
Jhon SM <Jhon SM@DESKTOP-RE6U8AU>
2024-03-05 13:32:03 -0500

    Carga 1 denuevo 2.0 (Jordan)

--- src/practica3/Practica3.java
+++ src/practica3/Practica3.java
@@ -16,6 +16,10 @@ public class Practica3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
+        System.out.println("Hello World!1");
+        System.out.println("Hello World!2");
+        System.out.println("Hello World!3");
+        System.out.println("Hello World!4");
     }
     
 }
